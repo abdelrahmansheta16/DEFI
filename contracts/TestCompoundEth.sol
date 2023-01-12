2a2ffc00f1533c119b9b7a25a66084a9c31ff2c7
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-01-12 08:56:42 +0200

    Update Uniswap test functionalities

---
+++ contracts/TestCompoundEth.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/compound.sol";
+
+contract TestCompoundEth {
+    CEth public cToken;
+
+    constructor(address _cToken) {
+        cToken = CEth(_cToken);
+    }
+}
