86374c56ff6e1b179cdac3992e1c7ef82b54fec8
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-01-24 09:08:19 +0200

    Update Uniswap test functionalities

--- test/test-compound-erc20-borrow.js
+++ test/test-compound-erc20-borrow.js
@@ -9,4 +9,17 @@ const IERC20 = artifacts.require("IERC20")
 const CErc20 = artifacts.require("CErc20")
 const TestCompoundErc20 = artifacts.require("TestCompoundErc20")
 
+contract("TestCompoundErc20", (accounts) => {
+  const WHALE = WBTC_WHALE
+  const TOKEN = WBTC
+  const C_TOKEN = CWBTC
+  const TOKEN_TO_BORROW = DAI
+  const C_TOKEN_TO_BORROW = CDAI
+  const REPAY_WHALE = DAI_WHALE // used to repay interest on borrow
 
+  const SUPPLY_DECIMALS = 8
+  const SUPPLY_AMOUNT = pow(10, SUPPLY_DECIMALS).mul(new BN(1))
+  const BORROW_DECIMALS = 18
+  const BORROW_INTEREST = pow(10, BORROW_DECIMALS).mul(new BN(1000))
+
+})
