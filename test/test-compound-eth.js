7b19ebbbf6eee4e905d133fee7f894ea971003c3
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-02-14 10:10:47 +0200

    Update Uniswap test functionalities

---
+++ test/test-compound-eth.js
@@ -0,0 +1,12 @@
+const { time } = require("@openzeppelin/test-helpers")
+const assert = require("assert")
+const BN = require("bn.js")
+const { sendEther, pow } = require("./util")
+const { CETH } = require("./config")
+const { web3 } = require("@openzeppelin/test-helpers/src/setup")
+
+const IERC20 = artifacts.require("IERC20")
+const CErc20 = artifacts.require("CErc20")
+const TestCompoundEth = artifacts.require("TestCompoundEth")
+
+const DEPOSIT_AMOUNT = pow(10, 18).mul(new BN(1))
