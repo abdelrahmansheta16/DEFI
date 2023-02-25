8c564e801810f1408026977fb8257fbe0eb80a34
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-02-25 10:18:30 +0200

    Update Defi functionalities

---
+++ test/test-compound-erc20.js
@@ -0,0 +1,12 @@
+const { time } = require("@openzeppelin/test-helpers")
+const assert = require("assert")
+const BN = require("bn.js")
+const { sendEther, pow } = require("./util")
+const { DAI, DAI_WHALE, CDAI, WBTC, WBTC_WHALE, CWBTC } = require("./config")
+const { web3 } = require("@openzeppelin/test-helpers/src/setup")
+
+const IERC20 = artifacts.require("IERC20")
+const CErc20 = artifacts.require("CErc20")
+const TestCompoundErc20 = artifacts.require("TestCompoundErc20")
+
+const DEPOSIT_AMOUNT = pow(10, 8).mul(new BN(1))
