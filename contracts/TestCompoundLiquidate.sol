78933e8e2d2523fd894f549d77f3064ac771ecb8
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-03-15 10:53:24 +0200

    Update Defi functionalities

---
+++ contracts/TestCompoundLiquidate.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/compound.sol";
+
+// supply
+// borrow max
+// wait few blocks and let borrowed balance > supplied balance * col factor
+// liquidate
+
+contract TestCompoundLiquidate {
+    Comptroller public comptroller =
+        Comptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
+
+    PriceFeed public priceFeed =
+        PriceFeed(0x922018674c12a7F0D394ebEEf9B58F186CdE13c1);
+
+    IERC20 public tokenSupply;
+    CErc20 public cTokenSupply;
+    IERC20 public tokenBorrow;
+    CErc20 public cTokenBorrow;
+
+    event Log(string message, uint val);
+}
