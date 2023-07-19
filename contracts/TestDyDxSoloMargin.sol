3889a91ee03efe6e56fb25a5bf9045331964e00c
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-07-19 09:14:48 +0300

    Update Uniswap test functionalities

--- contracts/TestDyDxSoloMargin.sol
+++ contracts/TestDyDxSoloMargin.sol
@@ -17,6 +17,42 @@ contract TestDyDxSoloMargin is ICallee, DydxFlashloanBase {
         address token;
         uint repayAmount;
     }
+
+    function initiateFlashLoan(address _token, uint _amount) external {
+        ISoloMargin solo = ISoloMargin(SOLO);
+
+        // Get marketId from token address
+        /*
+    0	WETH
+    1	SAI
+    2	USDC
+    3	DAI
+    */
+        uint marketId = _getMarketIdFromTokenAddress(SOLO, _token);
+
+        // Calculate repay amount (_amount + (2 wei))
+        uint repayAmount = _getRepaymentAmountInternal(_amount);
+        IERC20(_token).approve(SOLO, repayAmount);
+
+        /*
+    1. Withdraw
+    2. Call callFunction()
+    3. Deposit back
+    */
+
+        Actions.ActionArgs[] memory operations = new Actions.ActionArgs[](3);
+
+        operations[0] = _getWithdrawAction(marketId, _amount);
+        operations[1] = _getCallAction(
+            abi.encode(MyCustomData({token: _token, repayAmount: repayAmount}))
+        );
+        operations[2] = _getDepositAction(marketId, repayAmount);
+
+        Account.Info[] memory accountInfos = new Account.Info[](1);
+        accountInfos[0] = _getAccountInfo();
+
+        solo.operate(accountInfos, operations);
+    }
 }
 // Solo margin contract mainnet - 0x1e0447b19bb6ecfdae1e4ae1694b0c3659614e4e
 // payable proxy - 0xa8b39829cE2246f89B31C013b8Cde15506Fb9A76
