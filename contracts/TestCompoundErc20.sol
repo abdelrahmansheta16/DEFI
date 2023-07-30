ae744303645859fac1039496460207d389996da2
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-07-30 09:40:14 +0300

    Update Uniswap test functionalities

--- contracts/TestCompoundErc20.sol
+++ contracts/TestCompoundErc20.sol
@@ -10,13 +10,23 @@ import "./interfaces/compound.sol";
 // redeem
 
 contract TestCompoundErc20 {
-    IERC20 public token;
-    CErc20 public cToken;
+  IERC20 public token;
+  CErc20 public cToken;
 
-    event Log(string message, uint val);
+  event Log(string message, uint val);
 
-    constructor(address _token, address _cToken) {
-        token = IERC20(_token);
-        cToken = CErc20(_cToken);
-    }
+  constructor(address _token, address _cToken) {
+    token = IERC20(_token);
+    cToken = CErc20(_cToken);
+  }
+
+  function supply(uint _amount) external {
+    token.transferFrom(msg.sender, address(this), _amount);
+    token.approve(address(cToken), _amount);
+    require(cToken.mint(_amount) == 0, "mint failed");
+  }
+
+
+
+
 }
