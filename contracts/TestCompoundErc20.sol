7bde9ef36d0258a87a547fd9d5bc25cefa7eb51a
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-08-01 09:41:29 +0300

    Update Uniswap test functionalities

--- contracts/TestCompoundErc20.sol
+++ contracts/TestCompoundErc20.sol
@@ -10,26 +10,31 @@ import "./interfaces/compound.sol";
 // redeem
 
 contract TestCompoundErc20 {
-  IERC20 public token;
-  CErc20 public cToken;
+    IERC20 public token;
+    CErc20 public cToken;
 
-  event Log(string message, uint val);
+    event Log(string message, uint val);
 
-  constructor(address _token, address _cToken) {
-    token = IERC20(_token);
-    cToken = CErc20(_cToken);
-  }
+    constructor(address _token, address _cToken) {
+        token = IERC20(_token);
+        cToken = CErc20(_cToken);
+    }
 
-  function supply(uint _amount) external {
-    token.transferFrom(msg.sender, address(this), _amount);
-    token.approve(address(cToken), _amount);
-    require(cToken.mint(_amount) == 0, "mint failed");
-  }
+    function supply(uint _amount) external {
+        token.transferFrom(msg.sender, address(this), _amount);
+        token.approve(address(cToken), _amount);
+        require(cToken.mint(_amount) == 0, "mint failed");
+    }
 
-  function getCTokenBalance() external view returns (uint) {
-    return cToken.balanceOf(address(this));
-  }
+    function getCTokenBalance() external view returns (uint) {
+        return cToken.balanceOf(address(this));
+    }
 
-
-
+    // not view function
+    function getInfo() external returns (uint exchangeRate, uint supplyRate) {
+        // Amount of current exchange rate from cToken to underlying
+        exchangeRate = cToken.exchangeRateCurrent();
+        // Amount added to you supply balance this block
+        supplyRate = cToken.supplyRatePerBlock();
+    }
 }
