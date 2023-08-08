b736ece9cb931d6bf9e9b703f8988ba8bf89bd66
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-08-08 09:51:08 +0300

    Update Uniswap test functionalities

--- contracts/TestCompoundErc20.sol
+++ contracts/TestCompoundErc20.sol
@@ -10,86 +10,90 @@ import "./interfaces/compound.sol";
 // redeem
 
 contract TestCompoundErc20 {
-  IERC20 public token;
-  CErc20 public cToken;
+    IERC20 public token;
+    CErc20 public cToken;
 
-  event Log(string message, uint val);
+    event Log(string message, uint val);
 
-  constructor(address _token, address _cToken) {
-    token = IERC20(_token);
-    cToken = CErc20(_cToken);
-  }
+    constructor(address _token, address _cToken) {
+        token = IERC20(_token);
+        cToken = CErc20(_cToken);
+    }
 
-  function supply(uint _amount) external {
-    token.transferFrom(msg.sender, address(this), _amount);
-    token.approve(address(cToken), _amount);
-    require(cToken.mint(_amount) == 0, "mint failed");
-  }
+    function supply(uint _amount) external {
+        token.transferFrom(msg.sender, address(this), _amount);
+        token.approve(address(cToken), _amount);
+        require(cToken.mint(_amount) == 0, "mint failed");
+    }
 
-  function getCTokenBalance() external view returns (uint) {
-    return cToken.balanceOf(address(this));
-  }
+    function getCTokenBalance() external view returns (uint) {
+        return cToken.balanceOf(address(this));
+    }
 
-  // not view function
-  function getInfo() external returns (uint exchangeRate, uint supplyRate) {
-    // Amount of current exchange rate from cToken to underlying
-    exchangeRate = cToken.exchangeRateCurrent();
-    // Amount added to you supply balance this block
-    supplyRate = cToken.supplyRatePerBlock();
-  }
+    // not view function
+    function getInfo() external returns (uint exchangeRate, uint supplyRate) {
+        // Amount of current exchange rate from cToken to underlying
+        exchangeRate = cToken.exchangeRateCurrent();
+        // Amount added to you supply balance this block
+        supplyRate = cToken.supplyRatePerBlock();
+    }
 
-  // not view function
-  function estimateBalanceOfUnderlying() external returns (uint) {
-    uint cTokenBal = cToken.balanceOf(address(this));
-    uint exchangeRate = cToken.exchangeRateCurrent();
-    uint decimals = 8; // WBTC = 8 decimals
-    uint cTokenDecimals = 8;
+    // not view function
+    function estimateBalanceOfUnderlying() external returns (uint) {
+        uint cTokenBal = cToken.balanceOf(address(this));
+        uint exchangeRate = cToken.exchangeRateCurrent();
+        uint decimals = 8; // WBTC = 8 decimals
+        uint cTokenDecimals = 8;
 
-    return (cTokenBal * exchangeRate) / 10**(18 + decimals - cTokenDecimals);
-  }
+        return
+            (cTokenBal * exchangeRate) / 10 ** (18 + decimals - cTokenDecimals);
+    }
 
-  // not view function
-  function balanceOfUnderlying() external returns (uint) {
-    return cToken.balanceOfUnderlying(address(this));
-  }
+    // not view function
+    function balanceOfUnderlying() external returns (uint) {
+        return cToken.balanceOfUnderlying(address(this));
+    }
 
-  function redeem(uint _cTokenAmount) external {
-    require(cToken.redeem(_cTokenAmount) == 0, "redeem failed");
-    // cToken.redeemUnderlying(underlying amount);
-  }
+    function redeem(uint _cTokenAmount) external {
+        require(cToken.redeem(_cTokenAmount) == 0, "redeem failed");
+        // cToken.redeemUnderlying(underlying amount);
+    }
 
-  // borrow and repay //
-  Comptroller public comptroller =
-    Comptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
+    // borrow and repay //
+    Comptroller public comptroller =
+        Comptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
 
-  PriceFeed public priceFeed = PriceFeed(0x922018674c12a7F0D394ebEEf9B58F186CdE13c1);
+    PriceFeed public priceFeed =
+        PriceFeed(0x922018674c12a7F0D394ebEEf9B58F186CdE13c1);
 
-  // collateral
-  function getCollateralFactor() external view returns (uint) {
-    (bool isListed, uint colFactor, bool isComped) = comptroller.markets(
-      address(cToken)
-    );
-    return colFactor; // divide by 1e18 to get in %
-  }
+    // collateral
+    function getCollateralFactor() external view returns (uint) {
+        (bool isListed, uint colFactor, bool isComped) = comptroller.markets(
+            address(cToken)
+        );
+        return colFactor; // divide by 1e18 to get in %
+    }
 
-  // account liquidity - calculate how much can I borrow?
-  // sum of (supplied balance of market entered * col factor) - borrowed
-  function getAccountLiquidity()
-    external
-    view
-    returns (uint liquidity, uint shortfall)
-  {
-    // liquidity and shortfall in USD scaled up by 1e18
-    (uint error, uint _liquidity, uint _shortfall) = comptroller.getAccountLiquidity(
-      address(this)
-    );
-    require(error == 0, "error");
-    // normal circumstance - liquidity > 0 and shortfall == 0
-    // liquidity > 0 means account can borrow up to `liquidity`
-    // shortfall > 0 is subject to liquidation, you borrowed over limit
-    return (_liquidity, _shortfall);
-  }
+    // account liquidity - calculate how much can I borrow?
+    // sum of (supplied balance of market entered * col factor) - borrowed
+    function getAccountLiquidity()
+        external
+        view
+        returns (uint liquidity, uint shortfall)
+    {
+        // liquidity and shortfall in USD scaled up by 1e18
+        (uint error, uint _liquidity, uint _shortfall) = comptroller
+            .getAccountLiquidity(address(this));
+        require(error == 0, "error");
+        // normal circumstance - liquidity > 0 and shortfall == 0
+        // liquidity > 0 means account can borrow up to `liquidity`
+        // shortfall > 0 is subject to liquidation, you borrowed over limit
+        return (_liquidity, _shortfall);
+    }
 
-
-
+    // open price feed - USD price of token to borrow
+    function getPriceFeed(address _cToken) external view returns (uint) {
+        // scaled up by 1e18
+        return priceFeed.getUnderlyingPrice(_cToken);
+    }
 }
