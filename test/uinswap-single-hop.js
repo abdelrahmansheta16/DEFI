86d3c8e6ae6a4639c7f47f0e896bb5c78a631c00
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2023-09-02 15:13:14 +0300

    Update Defi functionalities

--- test/uinswap-single-hop.js
+++ test/uinswap-single-hop.js
@@ -6,5 +6,10 @@ const WETH9 = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
 
 describe("SwapExamples", () => {
+    let swapExamples
+    let accounts
+    let weth
+    let dai
+    let usdc
 
 })
